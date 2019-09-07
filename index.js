4cbf422a3331f714c6b0a1a8546b3c23c7046176
Briii <brianwkhuffman@gmail.com>
2019-09-07 11:07:29 -1000

    Finished

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function locate(array, target) {
   // do work here
-}
+  let newArr = JSON.stringify(array);
+  let newTarget = JSON.stringify(target);
+  let result = newArr.indexOf(newTarget);
+  if (result != -1) {
+    return true;
+  } else {
+    return false;
+  }
+
+};
 
 module.exports = locate;
